76afe980233fcd3ec18f879fe0091a802f24220e
zarque <zaonono@hotmail.fr>
2016-10-05 21:22:13 +0200

    Google A

--- require/html.php
+++ require/html.php
@@ -43,15 +43,24 @@
   ?>
 
   		<!-- Scripts -->
-  			<script src="<?echo BaseUrl?>/assets/js/jquery.min.js"></script>
-  			<script src="<?echo BaseUrl?>/assets/js/jquery.scrollex.min.js"></script>
-  			<script src="<?echo BaseUrl?>/assets/js/jquery.scrolly.min.js"></script>
-  			<script src="<?echo BaseUrl?>/assets/js/skel.min.js"></script>
-  			<script src="<?echo BaseUrl?>/assets/js/particles.min.js"></script>
-  			<script src="<?echo BaseUrl?>/assets/js/util.js"></script>
+  			<script src="<?echo BaseUrl?>/assets/js/jquery.min.js" async></script>
+  			<script src="<?echo BaseUrl?>/assets/js/jquery.scrollex.min.js" async></script>
+  			<script src="<?echo BaseUrl?>/assets/js/jquery.scrolly.min.js" async></script>
+  			<script src="<?echo BaseUrl?>/assets/js/skel.min.js" async></script>
+  			<script src="<?echo BaseUrl?>/assets/js/particles.min.js" async></script>
+  			<script src="<?echo BaseUrl?>/assets/js/util.js" async></script>
   			<!--[if lte IE 8]><script src="<?echo BaseUrl?>/assets/js/ie/respond.min.js"></script><![endif]-->
-  			<script src="<?echo BaseUrl?>/assets/js/main.js"></script>
+  			<script src="<?echo BaseUrl?>/assets/js/main.js" async></script>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+          ga('create', 'UA-42595785-8', 'auto');
+          ga('send', 'pageview');
+
+        </script>
 <?
   $Page->load(); //Load all assets (set in php file of page)
 ?>
