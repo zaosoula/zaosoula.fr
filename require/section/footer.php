ffac89206d5a5b3bc6dbf573358b9fe79f67dc04
zarque <zaonono@hotmail.fr>
2016-10-19 15:54:32 +0200

    Link profiles to BDD

--- require/section/footer.php
+++ require/section/footer.php
@@ -4,12 +4,11 @@
 	<section id="contact">
 		<h2>Me contacter</h2>
 		<ul class="icons">
-			<li><a href="https://twitter.com/zarque7" target="_blank" class="icon fa-twitter alt"><span class="label">Twitter</span></a></li>
-			<li><a href="https://www.facebook.com/Zao-Soula-142126902851327" target="_blank" class="icon fa-facebook alt"><span class="label">Facebook</span></a></li>
-			<li><a href="https://instagram.com/zarque7" target="_blank" class="icon fa-instagram alt"><span class="label">Instagram</span></a></li>
-			<li><a href="https://github.com/zarque" target="_blank" class="icon fa-github alt"><span class="label">GitHub</span></a></li>
-			<li><a href="https://linkedin.com/in/zaosoula" target="_blank" class="icon fa-linkedin alt"><span class="label">Linkedin</span></a></li>
-			<li><a href="https://youtube.com/zaosoula" target="_blank" class="icon fa-youtube alt"><span class="label">Youtube</span></a></li>
+			<?
+				foreach($resume->basics['profiles'] as $key => $value){
+						echo '<li><a href="'.$value['url'].'" target="_blank" class="icon fa-'.strtolower($value['network']).' alt"><span class="label">'.$value['network'].'</span></a></li>';
+				}
+			?>
 		</ul>
 	</section>
 	<p class="copyright">&copy; Zao Soula. Design: <a href="https://html5up.net">HTML5 UP</a> & <a href="https://zaosoula.fr">Zao Soula.</a></p>
